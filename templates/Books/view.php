0c81367b3ae6d706e0a28872fac2a0818706c492
apurbzz-1994 <akash95august@gmail.com>
2022-08-31 02:28:53 +1000

    style book view page and add insights

--- templates/Books/view.php
+++ templates/Books/view.php
@@ -1,76 +1,58 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Book $book
  */
 ?>
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Book'), ['action' => 'edit', $book->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Book'), ['action' => 'delete', $book->id], ['confirm' => __('Are you sure you want to delete # {0}?', $book->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Books'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Book'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+    <!--Book image and CRUD functions-->
+    <div class="col-12 col-md-4 col-lg-4">
+        <img src="https://covers.openlibrary.org/b/isbn/<?= $book->isbn ?>-M.jpg" alt="<?= $book->name ?> book" class="img-thumbnail rounded mx-auto d-block">
+        <!--actions-->
+        <div style="text-align: center; margin-top: 2em;">
+            <?= $this->Html->link(__('Edit Book'), ['action' => 'edit', $book->id], ['class' => 'btn btn-info btn-sm']) ?>
+            <?= $this->Form->postLink(__('Delete Book'), ['action' => 'delete', $book->id], ['confirm' => __('Are you sure you want to delete # {0}?', $book->id), 'class' => 'btn btn-danger btn-sm']) ?>
         </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="books view content">
-            <h3><?= h($book->name) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Name') ?></th>
-                    <td><?= h($book->name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Author') ?></th>
-                    <td><?= h($book->author) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Description') ?></th>
-                    <td><?= h($book->description) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Category') ?></th>
-                    <td><?= $book->has('category') ? $this->Html->link($book->category->name, ['controller' => 'Categories', 'action' => 'view', $book->category->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($book->id) ?></td>
-                </tr>
-            </table>
-            <div class="related">
-                <h4><?= __('Related Users') ?></h4>
-                <?php if (!empty($book->users)) : ?>
-                <div class="table-responsive">
-                    <table>
-                        <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('Email') ?></th>
-                            <th><?= __('Password') ?></th>
-                            <th><?= __('Role') ?></th>
-                            <th><?= __('Created') ?></th>
-                            <th><?= __('Modified') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
-                        </tr>
-                        <?php foreach ($book->users as $users) : ?>
-                        <tr>
-                            <td><?= h($users->id) ?></td>
-                            <td><?= h($users->email) ?></td>
-                            <td><?= h($users->password) ?></td>
-                            <td><?= h($users->role) ?></td>
-                            <td><?= h($users->created) ?></td>
-                            <td><?= h($users->modified) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'Users', 'action' => 'view', $users->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'Users', 'action' => 'edit', $users->id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Users', 'action' => 'delete', $users->id], ['confirm' => __('Are you sure you want to delete # {0}?', $users->id)]) ?>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </table>
+    </div>
+    <!--book information-->
+    <div class="col-12 col-md-8 col-lg-8">
+        <h3><?= h($book->name) ?></h3>
+        <table class="table">
+            <tr>
+                <th><?= __('Name') ?></th>
+                <td><?= h($book->name) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Author') ?></th>
+                <td><?= h($book->author) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Description') ?></th>
+                <td><?= h($book->description) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Category') ?></th>
+                <td><span class="badge badge-dark"><?= h($book->category->name) ?></span></td>
+            </tr>
+        </table>
+        <div style = "margin-top: 3em">
+            <!--insights-->
+            <h5>Did you know?</h5>
+
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <div class="card" style="padding: 1em; text-align:center;">
+                        <div class="card-body">
+                            <p class="card-text">Number of readers added this book to their nook </p>
+                            <hr>
+                            <?php
+                            $userCount = count($book->users);
+                            ?>
+                            <h3 class="card-title"><?= $userCount ?></h3>
+                        </div>
+                    </div>
                 </div>
-                <?php endif; ?>
             </div>
         </div>
     </div>
